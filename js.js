4d49cd76326b67f346e9d7c2f98819ca53dc7c1c
Jay <jevonte_n@outlook.com>
2018-12-14 10:16:49 -0500

    my portfolio

---
+++ js.js
@@ -0,0 +1,22 @@
+// var pics = ["assets\images\taxpromarket.PNG", "assets\images\smashquestionaire.PNG", "assets\images\musictopia.PNG", "assets\images\musicgo.png" , "assets\images\marioClicky.PNG"];
+
+// for (i = 0; i < pics.length; i++) {
+
+//     var portfolio = $('<div>');
+
+//     portfolio.addClass("images");
+
+//     portfolio.append('<img src="' + pics[i] + '">');
+
+//     $('.portfolio').append(portfolio);
+
+
+
+    
+// console.log(pics[i]);
+   
+
+
+
+// }
+
